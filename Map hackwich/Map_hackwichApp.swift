96a44398e60058b7afae60bc41e1edbf62d57c9c
Ela Murat <ela.murat@icloud.com>
2022-07-27 09:45:38 -0500

    Initial Commit

---
+++ Map hackwich/Map_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_hackwichApp.swift
+//  Map hackwich
+//
+//  Created by Ela Murat on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Map_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
