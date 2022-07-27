165f97b9d1f43ba094dc6ac0e49f7c38ed36a6be
Ela Murat <ela.murat@icloud.com>
2022-07-27 10:11:18 -0500

    Added user location

---
+++ Map hackwich/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  Map hackwich
+//
+//  Created by Ela Murat on 27/07/2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager:NSObject,CLLocationManagerDelegate, ObservableObject {
+ var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
